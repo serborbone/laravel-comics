a7e99d3843f37acd9ec511f23d6634559644f2ae
Sergio Borbone <sergioborbone@gmail.com>
2022-03-28 16:34:27 +0200

    pack update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Ciao
                 </div>
 
                 <div class="links">
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('homepage');
 });
+
+/*Route::get('/', function() {
+    return 'La mia homepage';
+});*/
